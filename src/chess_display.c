44ed7f8e85ea193f8b6e804ec20c5b508b9174d2
jocomst <johncomstock9716@gmail.com>
2023-11-13 12:33:58 -0500

    adding print piece function

--- src/chess_display.c
+++ src/chess_display.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+// ANSI color codes
+#define RESET_COLOR "\033[0m"
+#define WHITE_PIECE_COLOR "\033[37m" // White color
+#define BLACK_PIECE_COLOR "\033[30m" // Black color
+
+void print_piece(char piece) {
+    if (piece >= 'A' && piece <= 'Z') {
+        // White piece - print in white color
+        printf(WHITE_PIECE_COLOR "%c" RESET_COLOR, piece);
+    } else if (piece >= 'a' && piece <= 'z') {
+        // Black piece - print in black color
+        printf(BLACK_PIECE_COLOR "%c" RESET_COLOR, piece);
+    } else {
+        // Empty square or invalid character - print as is
+        printf("%c", piece);
+    }
+}
+
 #define BOARD_SIZE 8  // Assuming you have defined BOARD_SIZE
 
 typedef struct {
