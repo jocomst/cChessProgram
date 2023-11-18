ac6fa79dcad67751787df74b849ff2f0ac660f20
jocomst <johncomstock9716@gmail.com>
2023-11-18 01:04:54 -0500

    fix segmentation fault

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 
 
 int main() {
-    char sample_pgn[] = "1.e4 e5 2.Nf3 Nc6 3.Bc4 Bc5";  // Example PGN
 
     Chessboard chessboard;
     initialize_board(&chessboard);  // This function is still in the chess_display.c file
