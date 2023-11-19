e52aeb789ee13a69e1bd7da933ba05682f14843a
jocomst <johncomstock9716@gmail.com>
2023-11-19 17:48:39 -0500

    beginning to add player input functionality

--- src/chess_reader.c
+++ src/chess_reader.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "chess_reader.h"
 
-void readPGNFile(const char *filename) {
-	FILE *file = fopen(filename, "r");
-	if (file == NULL) {
-		printf("Error:Unable to open file %s\n", filename);
-		exit(1);
-	}
-
-	char line[256];
-	while (fgets(line, sizeof(line), file)) {
-		printf("%s", line);
-	}
-
-	fclose(file);
+// Function to read the player's move in algebraic notation
+void read_player_move(char *input) {
+    printf("Enter your move (in algebraic notation): ");
+    // Assuming that the input will not exceed 99 characters.
+    // fgets reads up to sizeof(input) - 1 characters and null-terminates the string.
+    if (fgets(input, 100, stdin) == NULL) {
+        printf("Error reading input.\n");
+        exit(1);
+    }
+    
+    // Optional: Remove newline character if present
+    size_t len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n') {
+        input[len - 1] = '\0';
+    }
 }
