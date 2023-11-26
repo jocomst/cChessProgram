3f2fc6668983aafb4017fedd65e544cb51cc7b31
jocomst <johncomstock9716@gmail.com>
2023-11-25 21:50:31 -0500

    added functionality to start game

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include "chess_display.h"
+#include "chess_game_state.h"
 #include "game_controller.h"
 
 
 int main() {
 
     Chessboard chessboard;
+    GameState gameState;
     initialize_board(&chessboard); 
     display_board(&chessboard);     // This function is still in the chess_display.c file
+    initialize_game_state(&gameState);
     start_game_loop(&gameState);
 
     return 0;
