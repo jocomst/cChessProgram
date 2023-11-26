397981feb09db88646ab3953dcecc1cd09d1fec6
jocomst <johncomstock9716@gmail.com>
2023-11-26 12:24:46 -0500

    adding functionality to start game

--- include/game_controller.h
+++ include/game_controller.h
@@ -4,6 +4,9 @@
 
 #include "chess_game_state.h"
 
+// Function to prompt the user to start the game
+bool prompt_start_game();
+
 void start_game_loop(GameState *gameState);
 
 #endif // GAME_CONTROLLER_H
