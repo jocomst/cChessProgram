94b6eefca880412ac3efad8cbf1264668478885c
jocomst <johncomstock9716@gmail.com>
2023-12-06 23:12:47 -0500

    added declarations to test header

---
+++ tests/include/tests_cases.h
@@ -0,0 +1,19 @@
+// test_cases.h
+
+#ifndef TEST_CASES_H
+#define TEST_CASES_H
+
+#include "chess_game_state.h"
+#include "game_controller.h"
+
+// Declarations of test functions from test_chess_game_state.c
+void test_initialize_board(void);
+void test_initialize_game_state(void);
+
+// Declarations of test functions from test_game_controller.c
+void test_prompt_start_game(void);
+
+// You can add more declarations as you create more test functions.
+
+#endif // TEST_CASES_H
+
