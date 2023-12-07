4c0901fb880e1b0a542f6fdbd612c470f7405226
jocomst <johncomstock9716@gmail.com>
2023-12-07 07:25:46 -0500

    adding includes to header

--- tests/include/tests_cases.h
+++ tests/include/tests_cases.h
@@ -1,8 +1,19 @@
-#include <CUnit/Basic.h>
+// test_cases.h
 
-// Declare test functions from both test files
+#ifndef TEST_CASES_H
+#define TEST_CASES_H
+
+#include "chess_game_state.h"
+#include "game_controller.h"
+
+// Declarations of test functions from test_chess_game_state.c
 void test_initialize_board(void);
 void test_initialize_game_state(void);
+
+// Declarations of test functions from test_game_controller.c
 void test_prompt_start_game(void);
 
+// You can add more declarations as you create more test functions.
+
+#endif // TEST_CASES_H
 
