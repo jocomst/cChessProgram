229e2992423883066fd533f047b4b1401c64fbf4
jocomst <johncomstock9716@gmail.com>
2023-12-07 07:13:36 -0500

    adding test suite functions

--- tests/include/tests_cases.h
+++ tests/include/tests_cases.h
@@ -1,19 +1,46 @@
-// test_cases.h
+#include <CUnit/Basic.h>
 
-#ifndef TEST_CASES_H
-#define TEST_CASES_H
-
-#include "chess_game_state.h"
-#include "game_controller.h"
-
-// Declarations of test functions from test_chess_game_state.c
+// Declare test functions from both test files
 void test_initialize_board(void);
 void test_initialize_game_state(void);
-
-// Declarations of test functions from test_game_controller.c
 void test_prompt_start_game(void);
 
-// You can add more declarations as you create more test functions.
+int main() {
+    // Initialize the CUnit test registry
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
 
-#endif // TEST_CASES_H
+    // Add the 'GameState' suite to the registry
+    CU_pSuite pGameStateSuite = CU_add_suite("GameStateTestSuite", NULL, NULL);
+    if (NULL == pGameStateSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Add the 'GameState' tests to the suite
+    if ((NULL == CU_add_test(pGameStateSuite, "test of initialize_board()", test_initialize_board)) ||
+        (NULL == CU_add_test(pGameStateSuite, "test of initialize_game_state()", test_initialize_game_state))) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Add the 'GameController' suite to the registry
+    CU_pSuite pGameControllerSuite = CU_add_suite("GameControllerTestSuite", NULL, NULL);
+    if (NULL == pGameControllerSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Add the 'GameController' tests to the suite
+    if (NULL == CU_add_test(pGameControllerSuite, "test of prompt_start_game()", test_prompt_start_game)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Run all tests using the CUnit Basic interface
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
 
