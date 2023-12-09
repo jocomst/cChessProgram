38c02116f4381724d66528b59003f9add87ddc37
jocomst <johncomstock9716@gmail.com>
2023-12-09 11:52:22 -0500

    changing chess input header

--- include/chess_reader.h
+++ include/chess_reader.h
@@ -3,10 +3,16 @@
 
 #include "chess_game_state.h"
 
+#define MAX_MOVE_NOTATION_LEN 6  // Length to accommodate longer notations like "e8=Q+"
+
+typedef struct {
+    char notation[MAX_MOVE_NOTATION_LEN];
+} Move;
+
 // Function to read the player's move in algebraic notation
 void read_player_move(char *input);
 
 // Function to parse the player's move from algebraic notation into board coordinates
-bool parse_algebraic_notation(const char *input, int *start_x, int *start_y, int *end_x, int *end_y);
+bool parse_algebraic_notation(const char *input, Move *move);
 
 #endif // CHESS_READER_H
