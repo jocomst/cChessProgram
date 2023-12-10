ec10445d39118afef98380327f067ca6f11a0705
jocomst <johncomstock9716@gmail.com>
2023-12-10 13:23:34 -0500

    add validation function definition

--- src/chess_reader.c
+++ src/chess_reader.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
+#include "stdbool.h"
 #include "stdlib.h"
 #include "string.h"
+#include "ctype.h"
 #include "chess_reader.h"
 
 // Function to read the player's move in algebraic notation
@@ -18,4 +20,67 @@ void read_player_move(char *input) {
     if (len > 0 && input[len - 1] == '\n') {
         input[len - 1] = '\0';
     }
+}
+
+bool parse_algebraic_notation(const char *input, Move *move) {
+    // Clear existing notation
+    memset(move->notation, 0, MAX_MOVE_NOTATION_LEN);
+
+    // Validate and copy the input into the move notation
+    // Example validation: Check if the input is a valid algebraic notation
+    if (isValidNotation(input)) {  // You need to implement isValidNotation
+        strncpy(move->notation, input, MAX_MOVE_NOTATION_LEN - 1);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isValidNotation(const char *notation) {
+    int len = strlen(notation);
+
+    // Basic validation: Length check
+    if (len < 2 || len > 6) {
+        return false;
+    }
+
+    // Validating different types of moves
+    if (islower(notation[0])) {  // Pawn moves and captures (e.g., "e4", "exd5")
+        if (len == 2 && isdigit(notation[1])) {
+            return true;  // Simple pawn move
+        }
+        if (len == 4 && notation[1] == 'x' && 
+            islower(notation[2]) && isdigit(notation[3])) {
+            return true;  // Pawn capture
+        }
+        if (len == 5 && notation[1] == 'x' && 
+            islower(notation[2]) && isdigit(notation[3]) && notation[4] == '+') {
+            return true;  // Pawn capture with check
+        }
+    } else if (strchr("KQBNR", notation[0])) {  // Piece moves (e.g., "Nf3")
+        if (len == 3 && islower(notation[1]) && isdigit(notation[2])) {
+            return true;  // Simple piece move
+        }
+        if (len == 4 && notation[1] == 'x' && 
+            islower(notation[2]) && isdigit(notation[3])) {
+            return true;  // Piece capture
+        }
+        if (len == 5 && notation[1] == 'x' && 
+            islower(notation[2]) && isdigit(notation[3]) && notation[4] == '+') {
+            return true;  // Piece capture with check
+        }
+        if (len == 6 && notation[1] == 'x' && 
+            islower(notation[2]) && isdigit(notation[3]) && notation[4] == '=' &&
+            strchr("QBNR", notation[5])) {
+            return true;  // Pawn promotion with capture
+        }
+    }
+
+    // Castling (e.g., "O-O", "O-O-O")
+    if ((strcmp(notation, "O-O") == 0) || (strcmp(notation, "O-O-O") == 0)) {
+        return true;
+    }
+
+    // If none of the conditions are met, return false
+    return false;
 }
