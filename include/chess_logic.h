6db9cfd98511a606bbc697c63c1a90d06d8114f2
jocomst <johncomstock9716@gmail.com>
2023-12-22 10:31:03 -0500

    added has_moved function declaration

--- include/chess_logic.h
+++ include/chess_logic.h
@@ -35,5 +35,6 @@ void move_piece(Chessboard *chessboard, int start_row, int start_col, int end_ro
 int is_move_legal(const Chessboard *chessboard, int start_row, int start_col, int end_row, int end_col);
 // Function to check if a square is occupied based on GameState and Move
 bool is_square_occupied(const GameState *gameState, const Move *move);
+bool has_piece_moved(const GameState *gameState, const Move *move);
 
 #endif // CHESS_LOGIC_H
