1a8f49295ea5cd725c87f4c867e009b4d91435e8
jocomst <johncomstock9716@gmail.com>
2023-12-23 11:28:44 -0500

    fixing test suite for updated validation

--- tests/test_chess_reader.c
+++ tests/test_chess_reader.c
@@ -19,10 +19,28 @@ void test_isValidNotation_invalid(void) {
     CU_ASSERT_FALSE(isValidNotation("kingside"));
 }
 
-void test_parse_algebraic_notation(void) {
+void test_parse_standard_notation_valid(void) {
     Move move;
-    CU_ASSERT_TRUE(parse_algebraic_notation("e4", &move));
-    CU_ASSERT_STRING_EQUAL(move.notation, "e4");
-    
-    CU_ASSERT_FALSE(parse_algebraic_notation("e9", &move));
+
+    // Test for a valid move
+    move = parse_standard_notation("e2e4");
+    CU_ASSERT_EQUAL(move.startCol, 'e' - 'a');
+    CU_ASSERT_EQUAL(move.startRow, '8' - '2');
+    CU_ASSERT_EQUAL(move.endCol, 'e' - 'a');
+    CU_ASSERT_EQUAL(move.endRow, '8' - '4');
+
+    // Add more tests for different valid moves
+}
+
+void test_parse_standard_notation_invalid(void) {
+    Move move;
+
+    // Test for an invalid move
+    move = parse_standard_notation("e9z3");
+    CU_ASSERT_EQUAL(move.startRow, -1);
+    CU_ASSERT_EQUAL(move.startCol, -1);
+    CU_ASSERT_EQUAL(move.endRow, -1);
+    CU_ASSERT_EQUAL(move.endCol, -1);
+
+    // Add more tests for different invalid moves
 }
